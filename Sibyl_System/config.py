e3213e64b45039cb57a8444ee1247b9f8b1a78ad
Awesome Rajkumar <71130660+Rajkumar-27@users.noreply.github.com>
2021-03-07 21:31:20 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -36,7 +36,7 @@ INSPECTORS = [1634756943]
 # These intergers below are real chatids.
 # Replace it with your own chatids or the userbot will not start.
 Sibyl_logs = -1001274776877
-Sibyl_approved_logs = 
+Sibyl_approved_logs = -1001274776877
 GBAN_MSG_LOGS = -1001293004442
 
 ########################################
